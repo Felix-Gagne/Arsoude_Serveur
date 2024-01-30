7236c28b4b2d2bbbbd6bfdc941ff03c285ad6f6f
Andy Ly <2157338@cegepmontpetit.ca>
2024-01-30 10:42:10 -0500

    FCT_Create

--- Arsoude-Backend/Services/TrailService.cs
+++ Arsoude-Backend/Services/TrailService.cs
@@ -38,7 +38,76 @@ namespace Arsoude_Backend.Services
         }
 
 
+        public async Task<Trail> CreateTrail(Trail trail)
+        {
 
+            if (trail == null)
+            {
+                throw new Exception("Create Trail: the trail is null");
+            }
+
+            if (_context.Trails == null)
+            {
+                throw new Exception("Create Trail: Entity set 'ApplicationDbContext.Trails'  is null.");
+            }
+
+            _context.Trails.Add(trail);
+            await _context.SaveChangesAsync();
+
+            return trail;
+        }
+
+        public async Task DeleteTrail(int id)
+        {
+            if (_context.Trails == null)
+            {
+                throw new Exception("Delete Trail: Entity set 'ApplicationDbContext.Trails'  is null.");
+            }
+            if (id == null)
+            {
+                throw new Exception("Delete Trail: Id in paramater is null.");
+
+            }
+            var trail = await _context.Trails.FindAsync(id);
+            if (trail == null)
+            {
+                throw new Exception("Delete Trail: the trail is null");
+            }
+
+            _context.Trails.Remove(trail);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Trail> GetTrail(int id)
+        {
+            if (id == null)
+            {
+                throw new Exception("Get Trail: Id in paramater is null.");
+
+            }
+            if (_context.Trails == null)
+            {
+                throw new Exception("Get Trail: Entity set 'ApplicationDbContext.Trails'  is null.");
+            }
+            var trail = await _context.Trails.FindAsync(id);
+            if (trail == null)
+            {
+                throw new Exception("Get Trail: the trail is null");
+            }
+            return trail;
+        }
+
+        public async Task<List<Trail>> GetTrails()
+        {
+            if (_context.Trails != null)
+            {
+                return await _context.Trails.ToListAsync();
+            }
+            else
+            {
+                throw new Exception("Get Trails: Entity set 'ApplicationDbContext.Trails'  is null.");
+            }
+        }
 
 
 
