76f29c034e987fd57d7987575aaee5c4fbd10c95
Vlad <2184803@cegepmontpetit.ca>
2024-02-13 09:52:36 -0500

    Test_Favoris : Les tests du favoris sont terminer

--- Arsoude-BackendTests/Services/TrailServiceTests.cs
+++ Arsoude-BackendTests/Services/TrailServiceTests.cs
@@ -17,6 +17,7 @@ using Arsoude_Backend.Models.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Arsoude_Backend.Data;
 using Microsoft.Extensions.Options;
+using Arsoude_Backend.Exceptions;
 
 namespace Arsoude_Backend.Services.Tests
 {
@@ -24,126 +25,5 @@ namespace Arsoude_Backend.Services.Tests
     public class TrailServiceTests
     {
 
-        DbContextOptions<ApplicationDbContext> options;
-
-        private User user;
-        private Trail trailInTheFavorite;
-        private Trail trailtIWantInTheFavorite;
-
-        public TrailServiceTests()
-        {
-            options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(databaseName: "TrailService")
-                .UseLazyLoadingProxies(true)
-                .Options;
-        }
-
-        [TestInitialize]
-        public void Init()
-        {
-            using ApplicationDbContext db = new ApplicationDbContext(options);
-
-            Coordinates coordinates = new Coordinates() { Id = 1, Latitude = 1, Longitude = 1 };
-            Coordinates coordinatesEnd = new Coordinates() { Id = 2, Latitude = 1, Longitude = 1 };
-            Coordinates simpleCoordinates = new Coordinates() { Id = 3, Latitude = 1, Longitude = 1 };
-
-
-
-            user = new User
-            {
-                LastName = "Simeon",
-                FirstName = "Du bois",
-                AreaCode = "asdasd",
-                IdentityUserId = "11111111-1111-1111-1111-111111111113",
-                FavouriteTrails = new List<UserFavoriteTrail>()
-            };
-
-            trailInTheFavorite = new Trail
-            {
-                Name = "Favorite",
-                Description = "Test",
-                Location = "Canada",
-                Type = TrailType.Pied,
-                ImageUrl = "https://media.timeout.com/images/105824352/image.jpg",
-                StartingCoordinates = coordinates,
-                EndingCoordinates = coordinatesEnd,
-            };
-
-            trailtIWantInTheFavorite = new Trail
-            {
-                Name = "Not in the favorite",
-                Description = "Test",
-                Location = "Canada",
-                Type = TrailType.Pied,
-                ImageUrl = "https://media.timeout.com/images/105824352/image.jpg",
-                StartingCoordinates = coordinates,
-                EndingCoordinates = coordinatesEnd,
-            };
-
-            UserFavoriteTrail userFavoriteTrail = new UserFavoriteTrail()
-            {
-                Id = 1,
-                UserId = user.Id,
-                TrailId = trailInTheFavorite.Id,
-            };
-
-            user.FavouriteTrails.Add(userFavoriteTrail);
-
-            db.AddRange(user);
-            db.AddRange(coordinates);
-            db.AddRange(coordinatesEnd);
-            db.AddRange(trailInTheFavorite); 
-            db.AddRange(trailtIWantInTheFavorite);
-            db.AddRange(userFavoriteTrail);
-            db.SaveChanges();
-            
-        }
-
-        [TestCleanup]
-        public void Dispose()
-        {
-            //TODO on efface les données de tests pour remettre la BD dans son état initial
-            using ApplicationDbContext db = new ApplicationDbContext(options);
-            db.RemoveRange(db.TrailUsers);
-            db.RemoveRange(db.Trails);
-            db.RemoveRange(db.Coordinates);
-            db.RemoveRange(db.UserFavoriteTrails); 
-            db.SaveChanges();
-        }
-
-        [TestMethod()]
-        public void SwictchVisibility_UserNotFound()
-        {
-
-        }
-
-        [TestMethod()]
-        public void SwictchVisibility_TrailNotFound()
-        {
-
-        }
-
-        [TestMethod()]
-        public void SwictchVisibility_NotOwner()
-        {
-
-        }
-
-        [TestMethod()]
-        public async Task addTrailToFavorite()
-        {
-            using ApplicationDbContext db = new ApplicationDbContext(options);
-          
-            TrailService trailService = new TrailService(db);
-
-            User currentUser = await db.TrailUsers.Where(x => x.IdentityUserId == user.IdentityUserId).FirstOrDefaultAsync();
-            Trail trail = await db.Trails.Where(x => x.Id == trailtIWantInTheFavorite.Id).FirstOrDefaultAsync();
-
-            await trailService.ControlTrailFavorite(currentUser, trail.Id);
-
-            User currentUserAfterAdd = await db.TrailUsers.Where(x => x.IdentityUserId == user.IdentityUserId).FirstOrDefaultAsync();
-
-            Assert.AreEqual(currentUserAfterAdd.FavouriteTrails.Count(), 2);
-        }
     }
 }
