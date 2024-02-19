5b9ae049c6de655663b9ab2b6006eda80118c9c1
Guillaume Gobeil-Bouchard <2028873@cegepmontpetit.ca>
2024-02-19 10:03:06 -0500

    Config: les méthodes post et get sont fait

--- Arsoude-Backend/Controllers/CommentsController.cs
+++ Arsoude-Backend/Controllers/CommentsController.cs
@@ -12,6 +12,7 @@ using Arsoude_Backend.Services;
 using Arsoude_Backend.Models.DTOs;
 using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 
 namespace Arsoude_Backend.Controllers
 {
@@ -31,9 +32,11 @@ namespace Arsoude_Backend.Controllers
         }
 
         [HttpGet("{TrailId}")]
-        public async Task<ActionResult> GetTrailComments(int TrailId) {
-            try { 
-            
+        public async Task<ActionResult> GetTrailComments(int TrailId)
+        {
+            try
+            {
+
                 List<Comments> comments = await _commentService.GetComments(TrailId);
                 return Ok(comments);
 
@@ -41,10 +44,11 @@ namespace Arsoude_Backend.Controllers
             catch (Exception ex)
             {
 
-                if (ex.GetType() == typeof(TrailNotFoundException)) {
+                if (ex.GetType() == typeof(TrailNotFoundException))
+                {
 
                     return NotFound();
-                
+
                 }
             }
 
@@ -54,20 +58,36 @@ namespace Arsoude_Backend.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> PostComment(CommentDTO comment) 
+        public async Task<ActionResult> PostComment(CommentDTO comment)
         {
             IdentityUser? user = await _userManager.FindByIdAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
-
-            User Currentuser = await _context.TrailUsers.Where(u => u.IdentityUserId == user.Id).FirstOrDefaultAsync();
-
-            return Ok();
-            if(user == null)
+            if (user == null)
             {
-                return Unauthorized();
+                return Unauthorized(new { Message = "Comment : Please Login" });
             }
 
+            try
+            {
+                await _commentService.PostComment(comment, user);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                if (ex.GetType() == typeof(NullReferenceException))
+                {
+
+                    return NotFound(new { Message = "Comment : No comment found" });
 
 
+
+                }
+                return BadRequest(new { Message = "Comment : une erreur innatendu est survenu" });
+
+
+
+
+            }
         }
     }
 }
--- Arsoude-Backend/Services/CommentService.cs
+++ Arsoude-Backend/Services/CommentService.cs
@@ -2,6 +2,8 @@
 using Arsoude_Backend.Exceptions;
 using Arsoude_Backend.Models;
 using Arsoude_Backend.Models.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Arsoude_Backend.Services
 {
@@ -32,13 +34,24 @@ namespace Arsoude_Backend.Services
             return trail.Comments;
         }
 
-        public async Task PostComment(CommentDTO comment) { 
+        public async Task PostComment(CommentDTO comment, IdentityUser user) {
+            User commentowner = await _context.TrailUsers.Where(u => u.IdentityUserId == user.Id).FirstOrDefaultAsync();
+            Trail trail = await _context.Trails.FindAsync(comment.Trailid);
+            if (trail == null)
+            {
+                throw new NullReferenceException();
+            }
+            Comments newCom = new Comments {
+            Id= 0,
+            userHasCompleted = true,
+            Date = DateTime.Now,
+            User = commentowner,
+            Text = comment.Message,
+            Trail = trail
             
-           
-            
-        
-        
-        
+            };
+            await _context.AddAsync(newCom);
+            await _context.SaveChangesAsync();
         } 
 
     }
