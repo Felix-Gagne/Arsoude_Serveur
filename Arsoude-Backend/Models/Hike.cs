a4075f3e71a89f7bc02728d55e422f14319521f5
Andy Ly <2157338@cegepmontpetit.ca>
2024-02-20 09:45:49 -0500

    FCT_AjoutClassHike

---
+++ Arsoude-Backend/Models/Hike.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Arsoude_Backend.Models
+{
+    public class Hike
+    {
+
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Time { get; set; }
+
+        public int Distance { get; set; }
+
+        [JsonIgnore]
+        public virtual Trail Trail { get; set; }
+
+        [JsonIgnore]
+        public virtual User User { get; set; }
+
+    }
+}
